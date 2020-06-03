093675ad64eff99371f5f934a2c44541e9ebe475
Surya Dentha <44351870+surden@users.noreply.github.com>
2020-06-03 09:46:42 +0800

    Awal Menu
    Membuat daftar barang, deskripsi barang dan class  produk

---
+++ KatalogToko/app/src/main/java/com/example/katalogtoko/Produk.java
@@ -0,0 +1,37 @@
+package com.example.katalogtoko;
+
+public class Produk {
+    private String nama;
+    private String harga;
+    private String gambar;
+    private String deskripsi;
+    private String spesifikasi;
+
+    public Produk(String dataNama, String dataHarga, String dataGambar, String dataDeskripsi, String dataSpesifikasi){
+        nama = dataNama;
+        harga = dataHarga;
+        gambar = dataGambar;
+        deskripsi = dataDeskripsi;
+        spesifikasi = dataSpesifikasi;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public String getGambar() {
+        return gambar;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public String getSpesifikasi() {
+        return spesifikasi;
+    }
+}
